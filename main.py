bd8fee834b7ee8184fd7c0ed4b45521f2e45efe1
joerovar <joseph.rodriguez.v@gmail.com>
2022-04-20 21:33:50 -0400

    check time intervals

--- main.py
+++ main.py
@@ -67,7 +67,11 @@ odt_stnb_5 = np.load('joseph_stnb.npy')
 stop_ids = np.load('joseph_select_area.npy')
 non_zero_idx = np.load('joseph_flatten_index.npy')
 
-
+day_3_4_tot = np.sum(odt_ground_5[:288*3], axis=-1)
+print(day_3_4_tot.shape)
+plt.plot(np.arange(len(day_3_4_tot)), day_3_4_tot)
+plt.show()
+plt.close()
 odt_ground_day = increase_interval_size(odt_ground_5, prev_interval_min=5, new_interval_min=60*24)
 odt_stnb_day = increase_interval_size(odt_stnb_5, prev_interval_min=5, new_interval_min=60*24)
 
@@ -80,6 +84,10 @@ tot_board_stnb_day = odt_stnb_day.sum(axis=-1)
 odt_ground_5_weekday = extract_weekday_odt(odt_ground_5, 5) # single day intervals (averaged over all weekdays)
 odt_stnb_5_weekday = extract_weekday_odt(odt_stnb_5, 5)
 
+# tot_5_weekday = np.sum(odt_ground_5_weekday, axis=-1)
+# plt.plot(np.arange(len(tot_5_weekday)), tot_5_weekday)
+# plt.show()
+# plt.close()
 odt_ground_60_weekday = increase_interval_size(odt_ground_5_weekday, prev_interval_min=5, new_interval_min=60)
 odt_stnb_60_weekday = increase_interval_size(odt_stnb_5_weekday, prev_interval_min=5, new_interval_min=60)
 
@@ -100,8 +108,15 @@ odt_rates_stnb_30_wkday = np.zeros(shape=(nz_odt_stnb_30_wkday.shape[0], nr_stop
 odt_rates_stnb_30_wkday[:, non_zero_idx] = nz_odt_stnb_30_wkday*2
 odt_rates_stnb_30_wkday = np.reshape(odt_rates_stnb_30_wkday, (odt_rates_stnb_30_wkday.shape[0], nr_stops, nr_stops))
 
-np.save('rt_20_odt_rates_30.npy', odt_rates_stnb_30_wkday)
-np.save('rt_20_demand_stops.npy', stop_ids)
+# np.save('rt_20_odt_rates_30.npy', odt_rates_stnb_30_wkday)
+# np.save('rt_20_demand_stops.npy', stop_ids)
+
+# outbound_stops = np.load('rt_20_out_stops.pkl', allow_pickle=True)
+# arr_rates = np.sum(odt_rates_stnb_30_wkday, axis=-1)
+# stop_ids_lst = list(stop_ids)
+# idx_outbound = [stop_ids_lst.index(int(s)) for s in outbound_stops]
+# outbound_arr_rates = arr_rates[2:32, idx_outbound]
+# print(np.sum(outbound_arr_rates, axis=-1))
 
 # plt.imshow(odt_ground_30_wkday[6*2], cmap='Greys')
 # plt.colorbar()
